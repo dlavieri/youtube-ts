fea219a48b08580aee152cb71cc433406c5127a5
david lavieri <dvlavieri@gmail.com>
2021-01-19 11:41:24 -0800

    set up gapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+const API_KEY = "";
+
 const App: React.FC = () => {
+  const [ apiConnected, setApiConnected ] = useState<boolean>(false);
+
+  const loadYoutubeApi = () => {
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/client.js";
+
+    script.onload = () => {
+      window.gapi.load('client', () => {
+        window.gapi.client.setApiKey(API_KEY);
+        window.gapi.client.load('youtube', 'v3', () => {
+          setApiConnected(true);
+        })
+      })
+    }
+  }
+
   return (
     <div className="App">
 
